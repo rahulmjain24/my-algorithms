6cadbc0a535de48f29ddc33099f209e970a1933f
Rahul Jain <rahulmjain24@outlook.com>
2022-02-09 17:53:14 +0530

    improved binary search

--- search_Binary.js
+++ search_Binary.js
@@ -1,29 +1,26 @@
 const newArr = [1, 3, 5, 6, 7, 8, 9, 15, 19, 25, 27, 29, 40, 42]
 
-function helper(harr, hvalue) {
+function binarySearch(harr, hvalue) {
     let start = 0;
     let end = harr.length - 1;
     let mid = Math.round(end / 2)
-    function binarySearch(arr, value) {
-        if (value === arr[mid]) return mid;
-        else if (end == mid || start == mid) {
-            if(value===arr[start]) return start;
-            return -1
-        }
-        else if (value < arr[mid]) {
-            end = mid
-            mid = Math.round(end / 2)
-            return binarySearch(arr, value)
-        } else if (value > arr[mid]) {
-            start = mid
-            mid = Math.round((end + mid) / 2)
-            return binarySearch(arr, value)
-        } else {
-            return -1;
-        }
 
+    function main(arr, value) {
+        if (value === arr[mid]) return mid;
+        else if (end == mid || start == mid) return -1
+        else if (value < arr[mid]) {
+            end = mid - 1
+            mid = Math.round(end / 2)
+            return main(arr, value)
+        } else if (value > arr[mid]) {
+            start = mid + 1
+            mid = Math.round((end + mid) / 2)
+            return main(arr, value)
+        }
+        return -1;
     }
-    return binarySearch(harr, hvalue)
+    return main(harr, hvalue)
 }
 
-console.log(helper(newArr,15))
+const index = binarySearch(newArr, 27)
+console.log(index)
