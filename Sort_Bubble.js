11a35220dda9e12908fb23b1bab9cea0da59a9a2
Rahul Jain <rahulmjain24@outlook.com>
2022-02-11 17:40:16 +0530

    optimized bubble sort

--- Sort_Bubble.js
+++ Sort_Bubble.js
@@ -1,8 +1,10 @@
-const newArr = [34, 21, 54,14,65,23];
+//const newArr = [34, 21, 54,14,65,23];
+const newArr = [8, 1, 2, 3, 4, 5];
 
 function bubbleSort(arr) {
-    let i = arr.length-1;
+    let i = arr.length - 1;
     let j = 0;
+    let noSwap = true;
 
     function swap(arr, ind1, ind2) {
         let temp = arr[ind1];
@@ -12,27 +14,30 @@ function bubbleSort(arr) {
 
     function sortRecursion(arr) {
         if (i > 0) {
-            
-            if(i===j-1){
-                j = 0
-                i--;  
-            }
-
-            if(i>j){
+            if (i > j) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
                     j++;
+                    noSwap = false;
                     return sortRecursion(arr);
                 }
                 j++
                 return sortRecursion(arr)
-            }   
-                     
+            }
+            if (i === j) {
+                if (noSwap) return arr;
+                j = 0
+                i--;
+                noSwap = true;
+            }
             j++;
             return sortRecursion(arr);
-        }        
+        }
+        return arr
     }
-    sortRecursion(arr)
+    return sortRecursion(arr)
 }
 
+bubbleSort(newArr)
 console.log(newArr)
+
